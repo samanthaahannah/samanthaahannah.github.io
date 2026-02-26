53193c37234690166a161d7932c90e3a908466fd
Samantha Hannah <47864044+samanthaahannah@users.noreply.github.com>
2026-02-26 03:27:34 +0000

    Update 404.html path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
-        notfound: '404.html'
+        notfound: 'public/404.html'
       }
     }
   }

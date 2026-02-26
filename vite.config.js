c655af99c3fa25bdb9a31ede17f85ec35a01ae70
Samantha Hannah <47864044+samanthaahannah@users.noreply.github.com>
2026-02-26 02:45:27 +0000

    Refactor Vite config to include build options

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: "/",
-  plugins: [react()]
-})
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
+      }
+    }
+  }
+}
